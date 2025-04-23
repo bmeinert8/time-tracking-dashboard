7b1541ce78de90abbe7ea35c40b5f3027437451c
bmeinert8 <brian.meinert1@gmail.com>
2025-04-23 09:31:18 -0400

    Added function to update cards with timeframe mapping.

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let activityData;
 function fetchData() {
   return fetch('./data.json')
     .then((response) => {
-      if (!response.ok) throw new Error('Error: Failed to fetch data');
+      if (!response.ok) throw new Error(`Error: ${response.status} ${response.statusText}`);
     
       return response.json();
     })
@@ -14,6 +14,50 @@ function fetchData() {
 
 fetchData().then((data) => {
   activityData = data;
-  
   console.log(activityData);
+  updateCards('weekly');
 });
+
+function updateCards(timeframe) {
+  if (!activityData) {
+    console.error('No activity data available');
+    return;
+  }
+  
+  //Mapping for previous timefram prefixes
+  const timeFrameMap = {
+    daily: 'Yesterday',
+    weekly: 'Last Week',
+    monthly: 'Last Month'
+  };
+
+  activityData.forEach((activity) => {
+    //get the title from the data
+    const activityTitle = activity.title;
+
+    //normalize the title to match data-activity attribute
+    const normalizedTitle = activityTitle.toLowerCase().replace(/\s+/g, '-');
+
+    //find the matching card using data-activity
+    const card = document.querySelector(`[data-activity="${normalizedTitle}"`);
+
+    if (!card) {
+      console.error(`Card not found for activity: ${activityTitle}`);
+      return;
+    }
+
+    //select the current and previous elements
+    const currentElement = card.querySelector('.js-current');
+    const previousElement = card.querySelector('.js-previous');
+
+    //Get the current and previous values for the timeframe
+    const currentHours = activity.timeframes[timeframe].current;
+    const previousHours = activity.timeframes[timeframe].previous;
+
+    //gereate the time on cards
+    currentElement.textContent = `${currentHours}hrs`;
+    previousElement.textContent = `${timeFrameMap[timeframe]} – ${previousHours}hrs`;
+  })
+}
+
+
